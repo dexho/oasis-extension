a07fc866b6fceb6e96f03abaacfd218adb893c92
AlenaK06 <54857769+AlenaK06@users.noreply.github.com>
2020-07-29 22:25:08 -0400

    attempts to setup nodejs

--- backend/users/index.js
+++ backend/users/index.js
@@ -1,32 +1,35 @@
 const express = require('express')
 const mysql = require('mysql');
+const {body, validationResult} = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+const path = require('path');
 const app = express()
 const port = 3000
-
+const public = path.join(__dirname, '../public')
 const connection = mysql.createConnection({
   host     : '18.216.0.138',
+  port     : '53380',
   user     : 'akim',
   password : '2020Oasis$',
-  database : 'test_akim'
+  database : 'mvp1'
 });
  
-connection.connect();
+// connection.connect();
 
-// connection.query(`
-// CREATE TABLE IF NOT EXISTS users (
-//     user_id BINARY(16) PRIMARY KEY,
-//     user_name VARCHAR(255) ,
-//     password_hash VARCHAR(255),
-//     location_id ,
-//     occupation_id
-// )
-// `)
+app.use(express.static(public))
 
-app.post('/users', (req, res) => {
-    connection.query('INSERT INTO users blah blah blah', function (error, results, fields) {
-        if (error) throw error;
-        
-    });
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(public, 'coming-soon.html'))
+})
+
+app.post('/user-email', (req, res) => {
+  console.log("hey")
+  body('email', 'invalid email').isEmail().normalizeEmail()
+
+  // connection.query('INSERT INTO Users ', function (error, results, fields) {
+  //     if (error) throw error;
+  //     console.log('connected');
+  // });
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
