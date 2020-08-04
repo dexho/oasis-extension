6bd2e67d131b4ac260a2e3dd41f8b5f183c9f59b
AlenaK06 <54857769+AlenaK06@users.noreply.github.com>
2020-08-03 22:02:44 -0400

    added node js
    working select query

---
+++ app.js
@@ -0,0 +1,24 @@
+var mysqlx = require('@mysql/xdevapi');
+var myTable;
+
+// Connect to server using a connection URL
+mysqlx
+  .getSession({
+	user: 'oasismysql',
+	password: '2020Oa$iS',
+	host: '18.216.0.138',
+	port: 33060
+  })
+  .then(function (session) {
+	// Accessing an existing table
+	myTable = session.getSchema('mvp1').getTable('Information');
+	return myTable
+    	.select(['info_id', 'url', 'text'])
+    	.where('info_id like 1')
+    	.execute(function (row) {
+      	console.log(row);
+    	});
+  })
+  .then(function (myResult) {
+	console.log(myResult);
+  });
