c9dda35bd7bc33d7649ee754acbb59bf662c0cc1
Desiree Ho <desireeho@berkeley.edu>
2020-09-14 15:04:51 -0700

    js and css for color shift
    Committing js and css files to the proper folder

--- js/shift.js
+++ js/shift.js
@@ -2,13 +2,37 @@ var hour = new Date().getHours();
 
 
 if (hour < 6 || hour > 20) { 
-    gradient = "linear-gradient(-15deg, #141414 0%, #2f455a 100%)";
+    //night scheme //#2f455a
+    gradient = "linear-gradient(-15deg, #141414 0%, #626F9E 100%)";
     document.getElementById("dash-wrapper").style.background = gradient;
-    document.getElementById("island-night").style.opacity = 0.85;
+    document.getElementById("island-night").style.opacity = 0.80;
     document.getElementById("island-day").style.opacity = 0;
     document.getElementById("date").style.color = "#768A9D";
     document.getElementById("month").style.color = "#768A9D";
     document.getElementById("date").style.opacity = 0.5;
-    document.getElementById("month").style.opacity = 0.4;
-    document.getElementById("location").style.opacity = 0.7;
+    document.getElementById("month").style.opacity = 0.3;
+    document.getElementById("location").style.opacity = 0.6;
+} else if (hour == 7 || hour == 19){
+    //golden hour, sunrise/sunset
+    gradient = "linear-gradient(-15deg, #0e1c26 0%, #a4c6b8 100%)";
+    document.getElementById("dash-wrapper").style.background = gradient;
+    document.getElementById("island-night").style.opacity = 0.20;
+    document.getElementById("island-day").style.opacity = 1;
+    document.getElementById("date").style.color = "#768A9D";
+    document.getElementById("month").style.color = "#768A9D";
+    document.getElementById("date").style.opacity = 0.6;
+    document.getElementById("month").style.opacity = 0.3;
+    document.getElementById("location").style.opacity = 0.5;
+
+} else if (hour == 6 || hour == 20){
+    //blue hour, before sunrise/after sunset
+    gradient = "linear-gradient(-15deg, #132535 15%, #ccb3d1 100%)";
+    document.getElementById("dash-wrapper").style.background = gradient;
+    document.getElementById("island-night").style.opacity = 0.6;
+    document.getElementById("island-day").style.opacity = 0.7;
+    document.getElementById("date").style.color = "#768A9D";
+    document.getElementById("month").style.color = "#768A9D";
+    document.getElementById("date").style.opacity = 0.8;
+    document.getElementById("month").style.opacity = 0.5;
+    document.getElementById("location").style.opacity = 0.6;
 }
