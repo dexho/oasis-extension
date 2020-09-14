d7789fdf7c31c713aa9e76970cf19424f117dbdf
Rafael <52586062+rafael1856@users.noreply.github.com>
2020-09-14 17:02:30 -0400

    Revert "Animated island, color shift, and improved layout"

--- js/date.js
+++ js/date.js
@@ -4,12 +4,7 @@
 // const time = thedate.getHours() + ":" + thedate.getMinutes();
 // console.log(today);
 
-const date = moment().format('ddd'+', ');
-const month = moment().format('MMM Do');
-const lowerDate = date.toLowerCase();
-const lowerMonth = month.toLowerCase();
+const date = moment().format('ddd'+','+' MMM Do');
 const time = moment().format('LT');
 
-const dateID = document.getElementById("date").innerHTML = lowerDate;
-const monthID = document.getElementById("month").innerHTML = lowerMonth;
-const timeID = document.getElementById("time").innerHTML = time;
+const dateId = document.getElementById("date").innerHTML = date;
