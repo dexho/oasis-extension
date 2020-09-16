bacdd44643abe0ac45727a9c3dfc017cad30432f
Desiree Ho <desireeho@berkeley.edu>
2020-09-16 11:58:32 -0700

    Fixed bug in pull-tab
    Stays right-aligned when resizing window and responsive to click

--- js/shift.js
+++ js/shift.js
@@ -2,7 +2,7 @@ var hour = new Date().getHours();
 
 
 if (hour < 6 || hour > 20) { 
-    //night scheme //#2f455a
+    //night scheme from 9 PM to 5 AM
     gradient = "linear-gradient(-15deg, #141414 0%, #626F9E 100%)";
     document.getElementById("dash-wrapper").style.background = gradient;
     document.getElementById("island-night").style.opacity = 0.80;
@@ -12,8 +12,9 @@ if (hour < 6 || hour > 20) {
     document.getElementById("date").style.opacity = 0.5;
     document.getElementById("month").style.opacity = 0.3;
     document.getElementById("location").style.opacity = 0.6;
+    // document.getElementById("semicircle").style.backgroundColor = "#141414";
 } else if (hour == 7 || hour == 19){
-    //golden hour, sunrise/sunset
+    //golden hour, sunrise/sunset, at 7 AM and 7 PM
     gradient = "linear-gradient(-15deg, #0e1c26 0%, #a4c6b8 100%)";
     document.getElementById("dash-wrapper").style.background = gradient;
     document.getElementById("island-night").style.opacity = 0.20;
@@ -25,7 +26,7 @@ if (hour < 6 || hour > 20) {
     document.getElementById("location").style.opacity = 0.5;
 
 } else if (hour == 6 || hour == 20){
-    //blue hour, before sunrise/after sunset
+    //blue hour, before sunrise/after sunset, at 6 AM and 8 PM
     gradient = "linear-gradient(-15deg, #132535 15%, #ccb3d1 100%)";
     document.getElementById("dash-wrapper").style.background = gradient;
     document.getElementById("island-night").style.opacity = 0.6;
